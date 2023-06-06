fd9e98741c50426b8a7d173f36a792fd9af164e4
+ <+>
2023-06-06 13:06:45 +0300

    faker for the objects

--- lib/main.py
+++ lib/main.py
@@ -2,6 +2,8 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, Integer, String,VARCHAR, Date, ForeignKey,Time
+from faker import Faker
+import random
 
 
 Base = declarative_base()
@@ -12,21 +14,19 @@ class Patient(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     age= Column(String)
-    gender= Column(String)
     contact_info = Column(VARCHAR)
     address= Column(String)
     appointments = relationship("Appointment", back_populates="patient")
 
-    def __init__(self, id, name, age, gender, contact_info, address, appointments):
+    def __init__(self, id, name, age, contact_info, address, appointments):
         self.name=name
         self.age=age
-        self.gender=gender
         self.contact_info=contact_info
         self.address=address
         self.appointments=appointments
-    
-    
 
+    def __rep__(self):
+        return f"{self.name}"
 
 #class appointment and its attribute
 class Appointment(Base):
@@ -41,6 +41,13 @@ class Appointment(Base):
     
     patient= relationship("Patient", back_populates="appointments")
     doctor= relationship("Doctor", back_populates="appointments")
+
+    def __init__(self, id, appointment_date, appointment_time, status, patient_id, doctor_id):
+        self.appointment_date=appointment_date
+        self.appointment_time=appointment_time
+        self.status=status
+        self.patient_id= patient_id
+        self.doctor_id=doctor_id
     
 #class patient and its attribute
 class Doctor(Base):
@@ -52,6 +59,11 @@ class Doctor(Base):
      contact_info= Column(String)
      appointments = relationship("Appointment", back_populates="doctor")
 
+     def __init__(self, id, name, specialization, contact_info, appointments):
+         self.name=name
+         self.specialization=specialization
+         self.contact_info=contact_info
+         self.appointments=appointments
 
 # Connect to the database
 engine = sqlalchemy.create_engine('sqlite:///linda_mama_care.db')
@@ -61,4 +73,28 @@ session = Session()
 Base.metadata.create_all(engine)
 
 
-#generate random data using faker
+#generate random data using faker to populate the appointments
+fake = Faker()
+
+patient1 = Patient(
+    id=1,
+    name=fake.name(),
+    age=str(random.randint(18, 65)),
+    contact_info=fake.phone_number(),
+    address=fake.address(),
+    appointments=[]
+)
+
+
+session.add(patient1)
+session.commit()
+
+#generate random data
+# patient1= Patient(1,'Ann doe',25,'07987373','Kibera','postnatal')
+
+# session.add(patient1)
+# session.commit()
+
+
+
+
