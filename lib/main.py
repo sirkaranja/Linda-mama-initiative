0236a4ab1affe3d6538d6bf8819d7b7c3745fdc5
+ <+>
2023-06-06 11:11:34 +0300

    classes and database attributes

--- lib/main.py
+++ lib/main.py
@@ -1,25 +1,56 @@
 import sqlalchemy
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String,VARCHAR, Date, ForeignKey,Time
+
 
 Base = declarative_base()
 
+#class patient and its attribute
 class Patient(Base):
     __tablename__ = 'patients'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    # appointment type
-    #vacination type
+    age= Column(String)
+    gender= Column(String)
+    contact_info = Column(VARCHAR)
+    address= Column(String)
+    appointments = relationship("Appointment", back_populates="patient")
+
+    def __init__(self, id, name, age, gender, contact_info, address, appointments):
+        self.name=name
+        self.age=age
+        self.gender=gender
+        self.contact_info=contact_info
+        self.address=address
+        self.appointments=appointments
+    
+    
 
 
-
+#class appointment and its attribute
 class Appointment(Base):
+    #appointment table and some of its attribute
     __tablename__ = 'appointments'
     id = Column(Integer, primary_key= True)
-    # patient_id
-    #vaccine type
-    #date
+    appointment_date= Column(Date)
+    appointment_time= Column(Time)
+    status= Column(String)
+    patient_id= Column(Integer, ForeignKey('patients.id'))
+    doctor_id= Column(Integer, ForeignKey('doctors.id'))
+    
+    patient= relationship("Patient", back_populates="appointments")
+    doctor= relationship("Doctor", back_populates="appointments")
+    
+#class patient and its attribute
+class Doctor(Base):
+     #Doctor table and some of its attribute
+     __tablename__ = 'doctors'
+     id= Column(Integer, primary_key=True)
+     name= Column(String)
+     specialization= Column(String)
+     contact_info= Column(String)
+     appointments = relationship("Appointment", back_populates="doctor")
 
 
 # Connect to the database
@@ -28,3 +59,6 @@ Session = sessionmaker(bind=engine)
 session = Session()
 # Create the tables
 Base.metadata.create_all(engine)
+
+
+#generate random data using faker
